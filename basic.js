c07ca86249adbe5956984f1ef244b637d4cef7d0
@khushiverma18 <147837115+khushiverma18@users.noreply.github.com>
2023-12-18 21:28:43 +0530

    Update basic.js

--- basic.js
+++ basic.js
@@ -1,27 +1,30 @@
 console.log("hii khushi");
-
+\\
 let a=45;
 let b=true;
 console.log(a+b)
 
+//
 let k=true;
 let d=BigInt("456")
 console.log(typeof k);
 console.log(k);
 console.log(typeof d);
 
+//
 let num1=45;
 let num2=23;
 let sum=num1+num2;
 console.log(sum);
 
+//
 let aim="khushi";
 let l=4;
 console.log(aim+l)
 console.log(typeof (aim+l))
 
+//
 let bin=4
 let pin=3
 console.log(++bin)
 console.log(pin++)
-

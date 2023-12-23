ff9adb8f42c6253ebe3567a70eb218c39b75f5ec
@khushiverma18 <147837115+khushiverma18@users.noreply.github.com>
2023-12-23 18:56:10 +0530

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,55 @@
+//logical opertor
+let s=50;
+if(s==10){
+    console.log('you are baby')
+}
+else if(s<20){
+    console.log('you are not able')
+}
+else{
+    console.log ('you are able')
+}
+
+
+//ternary opertor
+let age=20;
+let min=(age<10?"yes":"no")
+console.log(min)
+
+
+//age b/w 10to20
+let agge=12;
+if(agge>10 && agge<20){
+    console.log("you are under")
+}
+else{
+    console.log("not under")
+}
+
+
+
+//swictch case
+let num=200;
+switch(num){
+    case 100:
+console.log("your num is 100")
+break
+ case 200:
+    console.log("your num is 200")
+    break
+    case 300:
+        console.log("your num is 300")
+        default:
+            console.log("not find")
+}
+
+
+
+
+
+
+
+
+
+
+

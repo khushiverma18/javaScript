ef9e5b34ece32c5d9cc2b3b107bb6c2b1ab8b84e
@khushiverma18 <147837115+khushiverma18@users.noreply.github.com>
2023-12-24 16:04:54 +0530

    Add files via upload

---
+++ time.js
@@ -0,0 +1,21 @@
+//setTimeout
+setTimeout(function(){
+    console.log("hii girls")
+},5000)
+
+//print yes if user enter y & if user enter n reply
+let a=setTimeout(function(){
+    console.log("yes,")
+},5000)
+let b=alert("enter your value")
+if('n'==b){
+    console.log(b)
+}
+else{
+    console.log(a)
+}
+
+//setInterval
+setInterval(function(){
+    console.log("plz do ")
+},5000)

0981db146f06d87323ee8cfebf755c426b792edb
@khushiverma18 <147837115+khushiverma18@users.noreply.github.com>
2024-02-10 20:17:23 +0530

    Add files via upload

---
+++ IIFE.js
@@ -0,0 +1,13 @@
+(function one(){
+    console.log('hlo mam')
+})();            //(; is requerd)
+
+//
+(()=>{
+    console.log('hii sir')
+})();
+
+//
+((name)=>{
+   console.log(`my name is ${name}`)
+})('khushi');
